28811528f9b4a045212b44f984e6aa271973ba28
njimmahdi@gmail.com <njimmahdi@gmail.com>
2016-06-09 03:38:03 +0300

    send email when 500 error

--- deploy.py
+++ deploy.py
@@ -39,6 +39,12 @@ try:
 	    s.prompt()
 	    #get the new code from git branch for this version we assume that you are either using ssh key or already stored your creds in server
 	    s.sendline("git pull origin "+data[target]['git_branch'])
+	    #update composer
+	    s.sendline("composer update --no-dev --optimize-autoloader")
+	    #update database
+	    s.sendline("php app/console doctrine:schema:update --force")
+	    #clear cache
+	    s.sendline("php app/console cache:clear --env=prod --no-debug")
 	    s.prompt()
 	    #production ready
 	    print("Build done")
